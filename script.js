33e2b4afe7338817d439a011cd6d681d17d03497
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-24 17:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function(){
+	var text1 = document.getElementById("text").value;
+	var outDiv = document.getElementById("output");
+
+	outDiv.innerHTML = " ";
+
+	setTimeout(function(){
+		outDiv.innerText = text1;
+	},1000);
+})
+
+document.getElementById("btn").addEventListener("click", function(){
+	var text2 = document.getElementById("delay").value;
+	var outDiv = document.getElementById("output");
+
+	outDiv.innerHTML = " ";
+
+	setTimeout(function(){
+		outDiv.innerText = text2;
+	},2000);
+})
